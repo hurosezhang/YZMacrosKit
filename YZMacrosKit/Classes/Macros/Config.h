e47ad561d25f4cc9db29434421d31f2cab369e72
jonzhang <jon.zhang@trustlife.com>
2020-11-23 14:03:07 +0800

    yzMacrosKit 第一版本0.1.0

---
+++ YZMacrosKit/Classes/Macros/Config.h
@@ -0,0 +1,15 @@
+//
+//  Config.h
+//  YZMacrosKit
+//
+//  Created by jonzhang on 2020/11/23.
+//  Copyright © 2020 hurosezhang. All rights reserved.
+//
+
+#ifndef Config_h
+#define Config_h
+
+#define YZtest   @"面对疾风吧"
+
+
+#endif /* Config_h */
